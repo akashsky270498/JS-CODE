4e9206a30c0c343189138928fc9dda5c0aa3d7db
Akash <akashsky270498@gmail.com>
2024-07-24 20:18:01 +0530

    variables and dataypes

---
+++ 1_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12341234;
+let accountName = "House of Spirits";
+var accountEmail = "spirits@gmail.com";
+accountState = "Chhattisgarh";
+let accountCity;
+
+console.log(accountId);
+console.table([accountId, accountName, accountEmail, accountState, accountCity]);
+console.table({accountId, accountName, accountEmail, accountState, accountCity});
+
+/*
+1. The value we define in const keyword cannot be changed.
+2. The value we define in let keyword can be changed further.
+3. The value we define in var keyword can be changed it is not recommended to use var keyword, as we have 
+issue of block scope & functional scope means if we change it will change the value everywhere where we 
+used the var variable with same name.
+4. If we just declare let keyword with no value it will store undefined value.
+5. if we will not use constant and variable to define any value it will take memory and get stored,
+but it is also not recommended.
+*/
---
+++ 2_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //this syntax verify the code written in file must be of newer version of JS.
+
+console.log(3 + 
+    3
+); //code readiability is a problem here
+
+console.log(3 + 3); console.log("Strict"); //here also problem with code rediability
+
+//alert(3+3) we are using nodejs not browser
+
+// Datatypes
+ /*
+1. number => hold only digits max value 2 to the power 53
+2. bigint => hold more than 2 to the power 53 used in stock like web app.
+3. string => for characters must be use under "" or ''.
+4. boolean => true or false
+5. symbol => used for finding the uniques.
+6. null => standalone value (when value is defined as null (empty)).
+7. undefined => when value is not defined.
+8. object 
+
+typeof keyword
+typeof keyword can be used in two ways.
+*/
+
+let age = 18;
+console.log(typeof age);
+console.log(typeof "House of Spirits");
