099d8025d45a7c11d8154acad7d74788134a5af5
Akash <akashsky270498@gmail.com>
2025-01-28 13:03:15 +0530

    learning events

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 console.log("Hitesh");
+
+console.log("Start");
+
+setImmediate(() => {
+    console.log("Set Immediate");
+});
+
+setTimeout(() => {
+    console.log("Set Timeout");
+}, 1000);
+
+console.log("End");
